8f4cc556ee81546ae2acc006aad3799801767830
kyuarl21 <kyuarl21@gmail.com>
2022-12-05 18:00:13 +0900

    Add bus route, bus station and bus position search code

--- src/main/java/kyu/pj/transportinformation/client/topis/TopisClient.java
+++ src/main/java/kyu/pj/transportinformation/client/topis/TopisClient.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
@@ -28,15 +29,207 @@ public class TopisClient {
                         .host(topisProperty.getHost())
                         .build().toUriString()
                 ).defaultHeaders(this::defaultHeaders)
+                .exchangeStrategies(exchangeStrategies)
                 .build();
     }
 
-    public String getRouteList(String stSrch) {
+    public String getRouteList(String search) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/busRouteInfo/getBusRouteList")
                         .queryParam("ServiceKey", topisProperty.getDecodingKey())
-                        .queryParam("stSrch", stSrch)
+                        .queryParam("stSrch", search)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getRouteInfo(String routeId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/busRouteInfo/getRouteInfo")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("busRouteId", routeId)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getRoutePath(String routeId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/busRouteInfo/getRoutePath")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("busRouteId", routeId)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getStationByRoute(String routeId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/busRouteInfo/getStaionByRoute")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("busRouteId", routeId)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getStationByName(String station) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/stationinfo/getStationByName")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("stSrch", station)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getStationById(String station) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/stationinfo/getStationByUid")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("arsId", station)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getRouteByStation(String stationId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/stationinfo/getRouteByStation")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("arsId", stationId)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getPositionByRouteId(String routeId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/buspos/getBusPosByRtid")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("busRouteId", routeId)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getPositionByStation(String routeId, String startStation, String endStation) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/buspos/getBusPosByRouteSt")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("busRouteId", routeId)
+                        .queryParam("startOrd", startStation)
+                        .queryParam("endOrd", endStation)
+                        .build()
+                ).exchangeToMono(response -> {
+                    if (response.statusCode().is2xxSuccessful()) {
+                        return response.bodyToMono(String.class);
+                    }
+                    return response.createException().flatMap(it -> {
+                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
+                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
+                    });
+                }).onErrorResume(error -> {
+                    log.debug("error : {}", error.getMessage());
+                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
+                }).block();
+    }
+
+    public String getPositionByBusId(String busId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/buspos/getBusPosByVehId")
+                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
+                        .queryParam("vehId", busId)
                         .build()
                 ).exchangeToMono(response -> {
                     if (response.statusCode().is2xxSuccessful()) {
@@ -73,28 +266,11 @@ public class TopisClient {
                 }).block();
     }
 
-    public String getStationByName(String stationName) {
-        return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/stationinfo/getStationByName")
-                        .queryParam("ServiceKey", topisProperty.getDecodingKey())
-                        .queryParam("stSrch", stationName)
-                        .build()
-                ).exchangeToMono(response -> {
-                    if (response.statusCode().is2xxSuccessful()) {
-                        return response.bodyToMono(String.class);
-                    }
-                    return response.createException().flatMap(it -> {
-                        String bodyAsString = it.getResponseBodyAsString(StandardCharsets.UTF_8);
-                        return Mono.error(new BadRequestException(111, bodyAsString)); // TODO
-                    });
-                }).onErrorResume(error -> {
-                    log.debug("error : {}", error.getMessage());
-                    return Mono.error(new BadRequestException(111, error.getMessage())); // TODO
-                }).block();
-    }
-
     private void defaultHeaders(HttpHeaders headers) {
         headers.add("content-type", "application/json; charset=utf-8");
     }
+
+    ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+            .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
+            .build();
 }
--- src/main/java/kyu/pj/transportinformation/topis/controller/TopisController.java
+++ src/main/java/kyu/pj/transportinformation/topis/controller/TopisController.java
@@ -4,10 +4,9 @@ import kyu.pj.transportinformation.topis.service.TopisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
 
 import static kyu.pj.transportinformation.common.utils.ResultUtils.resultOk;
 
@@ -20,20 +19,65 @@ public class TopisController {
     private final TopisService topisService;
 
     @GetMapping("route-list")
-    public ResponseEntity<?> getRouteList(@RequestParam(value = "name", required = false) String stSrch) {
-        String result = topisService.getRouteList(stSrch);
+    public ResponseEntity<?> getRouteList(@RequestParam(value = "search", required = false) String search) {
+        String result = topisService.getRouteList(search);
+        return resultOk(result);
+    }
+
+    @GetMapping("route-info")
+    public ResponseEntity<?> getRouteInfo(@RequestParam(value = "route_id", required = false) String routeId) {
+        String result = topisService.getRouteList(routeId);
+        return resultOk(result);
+    }
+
+    @GetMapping("route-path")
+    public ResponseEntity<?> getRoutePath(@RequestParam(value = "route_id", required = false) String routeId) {
+        String result = topisService.getRoutePath(routeId);
+        return resultOk(result);
+    }
+
+    @GetMapping("station-route")
+    public ResponseEntity<?> getStationByRoute(@RequestParam(value = "route_id", required = false) String routeId) {
+        String result = topisService.getStationByRoute(routeId);
+        return resultOk(result);
+    }
+
+    @GetMapping("station/{station}")
+    public ResponseEntity<?> getStation(@PathVariable("station") @NotBlank(message = "정류소 정보는 필수입니다.") String station) {
+        String result = topisService.getStation(station);
+        return resultOk(result);
+    }
+
+    @GetMapping("route-station")
+    public ResponseEntity<?> getRouteByStation(@RequestParam(value = "station_id", required = false) String stationId) {
+        String result = topisService.getRouteByStation(stationId);
+        return resultOk(result);
+    }
+
+    @GetMapping("position-route")
+    public ResponseEntity<?> getPositionByRouteId(@RequestParam(value = "route_id", required = false) String routeId) {
+        String result = topisService.getPositionByRouteId(routeId);
+        return resultOk(result);
+    }
+
+    @GetMapping("position-station")
+    public ResponseEntity<?> getPositionByStation(
+            @RequestParam(value = "route_id", required = false) String routeId,
+            @RequestParam(value = "start_station", required = false) String startStation,
+            @RequestParam(value = "end_station", required = false) String endStation) {
+        String result = topisService.getPositionByStation(routeId, startStation, endStation);
+        return resultOk(result);
+    }
+
+    @GetMapping("position-bus")
+    public ResponseEntity<?> getPositionByBusId(@RequestParam(value = "bus_id", required = false) String busId) {
+        String result = topisService.getPositionByBusId(busId);
         return resultOk(result);
     }
 
     @GetMapping("arrive")
-    public ResponseEntity<?> getBusArrive(@RequestParam(value = "name", required = false) String routeId) {
+    public ResponseEntity<?> getBusArrive(@RequestParam(value = "route_id", required = false) String routeId) {
         String result = topisService.busArrive(routeId);
-        return resultOk(result);
-    }
-
-    @GetMapping("station")
-    public ResponseEntity<?> getStation(@RequestParam(value = "name", required = false) String stationName) {
-        String result = topisService.getStationByName(stationName);
         return resultOk(result);
     }
 }
